247eba079b5361b9f36fd64f60f7a05d4add336f
Wez Furlong <wez@wezfurlong.org>
2024-05-22 05:58:52 -0700

    DRY for Satellite and ZoneGroupMember types

--- src/zone.rs
+++ src/zone.rs
@@ -29,8 +29,15 @@ impl ZoneGroup {
     }
 }
 
+/// Helper for DRY; Satellite and ZoneGroupMember are almost
+/// identical structs but have to be separate in order for
+/// instant_xml to generate appropriate serde logic
+macro_rules! machine_info {
+    (pub struct $ty:ident { $($inner:tt)* }) => {
 #[derive(Debug, FromXml)]
-pub struct ZoneGroupMember {
+pub struct $ty {
+    $($inner)*
+
     #[xml(rename = "UUID", attribute)]
     pub uuid: String,
     /// URL of the device_description.xml
@@ -88,69 +95,19 @@ pub struct ZoneGroupMember {
     pub ssl_port: u16,
     #[xml(rename = "HHSSLPort", attribute)]
     pub hhssl_port: u16,
-
-    pub satellites: Vec<Satellite>,
+}
+    };
 }
 
-#[derive(Debug, FromXml)]
-pub struct Satellite {
-    #[xml(rename = "UUID", attribute)]
-    pub uuid: String,
-    /// URL of the device_description.xml
-    #[xml(rename = "Location", attribute)]
-    pub location: String,
-    #[xml(rename = "ZoneName", attribute)]
-    pub zone_name: String,
-    #[xml(rename = "Icon", attribute)]
-    pub icon: String,
-    #[xml(rename = "Configuration", attribute)]
-    pub configuration: String,
-    #[xml(rename = "SoftwareVersion", attribute)]
-    pub software_version: String,
-    #[xml(rename = "SWGen", attribute)]
-    pub sw_gen: String,
-    #[xml(rename = "MinCompatibleVersion", attribute)]
-    pub min_compatible_version: String,
-    #[xml(rename = "LegacyCompatibleVersion", attribute)]
-    pub legacy_compatible_version: String,
-    #[xml(rename = "BootSeq", attribute)]
-    pub boot_seq: String,
-    #[xml(rename = "TVConfigurationError", attribute)]
-    pub tv_configuration_error: String,
-    #[xml(rename = "HdmiCecAvailable", attribute)]
-    pub hdmi_cec_available: u8,
-    #[xml(rename = "WirelessMode", attribute)]
-    pub wireless_mode: u8,
-    #[xml(rename = "WirelessLeafOnly", attribute)]
-    pub wireless_leaf_only: u8,
-    #[xml(rename = "ChannelFreq", attribute)]
-    pub channel_freq: u32,
-    #[xml(rename = "BehindWifiExtender", attribute)]
-    pub behind_wifi_extender: u8,
-    #[xml(rename = "WifiEnabled", attribute)]
-    pub wifi_enabled: u8,
-    #[xml(rename = "EthLink", attribute)]
-    pub eth_link: u8,
-    #[xml(rename = "Orientation", attribute)]
-    pub orientation: u8,
-    #[xml(rename = "RoomCalibrationState", attribute)]
-    pub room_calibration_state: u32,
-    #[xml(rename = "SecureRegState", attribute)]
-    pub secure_reg_state: u32,
-    #[xml(rename = "VoiceConfigState", attribute)]
-    pub voice_config_state: u32,
-    #[xml(rename = "MicEnabled", attribute)]
-    pub mic_enabled: u8,
-    #[xml(rename = "AirPlayEnabled", attribute)]
-    pub airplay_enabled: u8,
-    #[xml(rename = "IdleState", attribute)]
-    pub idle_state: u8,
-    #[xml(rename = "MoreInfo", attribute)]
-    pub more_info: String,
-    #[xml(rename = "SSLPort", attribute)]
-    pub ssl_port: u16,
-    #[xml(rename = "HHSSLPort", attribute)]
-    pub hhssl_port: u16,
+machine_info! {
+    pub struct ZoneGroupMember {
+        pub satellites: Vec<Satellite>,
+    }
+}
+
+machine_info! {
+    pub struct Satellite {
+    }
 }
 
 #[cfg(test)]
@@ -170,6 +127,7 @@ mod test {
         id: "RINCON_XXX:3435548679",
         members: [
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.161:1400/xml/device_description.xml",
                 zone_name: "Primary Bath",
@@ -198,7 +156,6 @@ mod test {
                 more_info: "RawBattPct:99,BattPct:100,BattChg:CHARGING,BattTmp:33",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
         ],
     },
@@ -207,34 +164,6 @@ mod test {
         id: "RINCON_XXX:3326086195",
         members: [
             ZoneGroupMember {
-                uuid: "RINCON_XXX",
-                location: "http://10.10.10.196:1400/xml/device_description.xml",
-                zone_name: "Some Room",
-                icon: "",
-                configuration: "1",
-                software_version: "78.1-52020",
-                sw_gen: "2",
-                min_compatible_version: "77.0-00000",
-                legacy_compatible_version: "58.0-00000",
-                boot_seq: "123",
-                tv_configuration_error: "0",
-                hdmi_cec_available: 1,
-                wireless_mode: 0,
-                wireless_leaf_only: 0,
-                channel_freq: 2437,
-                behind_wifi_extender: 0,
-                wifi_enabled: 1,
-                eth_link: 0,
-                orientation: 0,
-                room_calibration_state: 1,
-                secure_reg_state: 3,
-                voice_config_state: 0,
-                mic_enabled: 0,
-                airplay_enabled: 1,
-                idle_state: 1,
-                more_info: "",
-                ssl_port: 1443,
-                hhssl_port: 1843,
                 satellites: [
                     Satellite {
                         uuid: "RINCON_XXX",
@@ -297,6 +226,34 @@ mod test {
                         hhssl_port: 1843,
                     },
                 ],
+                uuid: "RINCON_XXX",
+                location: "http://10.10.10.196:1400/xml/device_description.xml",
+                zone_name: "Some Room",
+                icon: "",
+                configuration: "1",
+                software_version: "78.1-52020",
+                sw_gen: "2",
+                min_compatible_version: "77.0-00000",
+                legacy_compatible_version: "58.0-00000",
+                boot_seq: "123",
+                tv_configuration_error: "0",
+                hdmi_cec_available: 1,
+                wireless_mode: 0,
+                wireless_leaf_only: 0,
+                channel_freq: 2437,
+                behind_wifi_extender: 0,
+                wifi_enabled: 1,
+                eth_link: 0,
+                orientation: 0,
+                room_calibration_state: 1,
+                secure_reg_state: 3,
+                voice_config_state: 0,
+                mic_enabled: 0,
+                airplay_enabled: 1,
+                idle_state: 1,
+                more_info: "",
+                ssl_port: 1443,
+                hhssl_port: 1843,
             },
         ],
     },
@@ -305,6 +262,7 @@ mod test {
         id: "RINCON_XXX:2302873263",
         members: [
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.166:1400/xml/device_description.xml",
                 zone_name: "Study",
@@ -333,7 +291,6 @@ mod test {
                 more_info: "TargetRoomName:Study",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
         ],
     },
@@ -342,6 +299,7 @@ mod test {
         id: "RINCON_XXX:4111376911",
         members: [
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.138:1400/xml/device_description.xml",
                 zone_name: "Beam",
@@ -370,7 +328,6 @@ mod test {
                 more_info: "",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
         ],
     },
@@ -379,6 +336,7 @@ mod test {
         id: "RINCON_XXX:2134456247",
         members: [
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.165:1400/xml/device_description.xml",
                 zone_name: "Kitchen (Move)",
@@ -407,7 +365,6 @@ mod test {
                 more_info: "RawBattPct:100,BattPct:100,BattChg:CHARGING,BattTmp:27",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
         ],
     },
@@ -416,34 +373,6 @@ mod test {
         id: "RINCON_XXX:2884078592",
         members: [
             ZoneGroupMember {
-                uuid: "RINCON_XXX",
-                location: "http://10.10.10.231:1400/xml/device_description.xml",
-                zone_name: "Primary Bedroom",
-                icon: "",
-                configuration: "1",
-                software_version: "78.1-52020",
-                sw_gen: "2",
-                min_compatible_version: "77.0-00000",
-                legacy_compatible_version: "58.0-00000",
-                boot_seq: "91",
-                tv_configuration_error: "0",
-                hdmi_cec_available: 1,
-                wireless_mode: 0,
-                wireless_leaf_only: 0,
-                channel_freq: 2437,
-                behind_wifi_extender: 0,
-                wifi_enabled: 1,
-                eth_link: 0,
-                orientation: 0,
-                room_calibration_state: 1,
-                secure_reg_state: 3,
-                voice_config_state: 0,
-                mic_enabled: 0,
-                airplay_enabled: 1,
-                idle_state: 1,
-                more_info: "",
-                ssl_port: 1443,
-                hhssl_port: 1843,
                 satellites: [
                     Satellite {
                         uuid: "RINCON_XXX",
@@ -536,6 +465,34 @@ mod test {
                         hhssl_port: 1843,
                     },
                 ],
+                uuid: "RINCON_XXX",
+                location: "http://10.10.10.231:1400/xml/device_description.xml",
+                zone_name: "Primary Bedroom",
+                icon: "",
+                configuration: "1",
+                software_version: "78.1-52020",
+                sw_gen: "2",
+                min_compatible_version: "77.0-00000",
+                legacy_compatible_version: "58.0-00000",
+                boot_seq: "91",
+                tv_configuration_error: "0",
+                hdmi_cec_available: 1,
+                wireless_mode: 0,
+                wireless_leaf_only: 0,
+                channel_freq: 2437,
+                behind_wifi_extender: 0,
+                wifi_enabled: 1,
+                eth_link: 0,
+                orientation: 0,
+                room_calibration_state: 1,
+                secure_reg_state: 3,
+                voice_config_state: 0,
+                mic_enabled: 0,
+                airplay_enabled: 1,
+                idle_state: 1,
+                more_info: "",
+                ssl_port: 1443,
+                hhssl_port: 1843,
             },
         ],
     },
@@ -544,6 +501,7 @@ mod test {
         id: "RINCON_XXX:1940091512",
         members: [
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.157:1400/xml/device_description.xml",
                 zone_name: "Great Room",
@@ -572,7 +530,6 @@ mod test {
                 more_info: "",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
         ],
     },
@@ -581,6 +538,7 @@ mod test {
         id: "RINCON_XXX:2667033389",
         members: [
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.120:1400/xml/device_description.xml",
                 zone_name: "Other Room",
@@ -609,9 +567,9 @@ mod test {
                 more_info: "",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.158:1400/xml/device_description.xml",
                 zone_name: "Other Room",
@@ -640,9 +598,9 @@ mod test {
                 more_info: "",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.217:1400/xml/device_description.xml",
                 zone_name: "Other Room",
@@ -671,7 +629,6 @@ mod test {
                 more_info: "",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
         ],
     },
@@ -680,6 +637,7 @@ mod test {
         id: "RINCON_XXX:97",
         members: [
             ZoneGroupMember {
+                satellites: [],
                 uuid: "RINCON_XXX",
                 location: "http://10.10.10.236:1400/xml/device_description.xml",
                 zone_name: "Kitchen",
@@ -708,7 +666,6 @@ mod test {
                 more_info: "",
                 ssl_port: 1443,
                 hhssl_port: 1843,
-                satellites: [],
             },
         ],
     },
